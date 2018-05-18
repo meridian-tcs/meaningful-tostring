9728809dbca1a106f6cb64c29ab412ac07714b63
Matt Middleton <matt.middleton@mtcs.org.uk>
2018-05-18 16:27:25 +0100

    Downgrading .NET Standard version to 1.. as it doesn't need to be 2!

--- Meridian.MeaningfulToString/Meridian.MeaningfulToString/ObjectExtensions.cs
+++ Meridian.MeaningfulToString/Meridian.MeaningfulToString/ObjectExtensions.cs
@@ -10,6 +10,7 @@
 namespace Meridian.MeaningfulToString
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
 
@@ -36,7 +37,8 @@ namespace Meridian.MeaningfulToString
 
             Type type = sender.GetType();
 
-            PropertyInfo[] propertyInfo = type.GetProperties();
+            IEnumerable<PropertyInfo> propertyInfo =
+                type.GetTypeInfo().DeclaredProperties;
 
             string[] propertyDescs = propertyInfo
                 .Select(x =>
