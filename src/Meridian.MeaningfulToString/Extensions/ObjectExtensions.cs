ccf8e8dc8a7b4e56a9ab9593055cdf988225f188
Matt Middleton <matt.middleton@mtcs.org.uk>
2024-12-18 11:55:32 +0000

    Project restructure, pipeline update and csproj update

--- src/Meridian.MeaningfulToString/Extensions/ObjectExtensions.cs
+++ src/Meridian.MeaningfulToString/Extensions/ObjectExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// ----------------------------------------------------------------------------
 // <copyright file="ObjectExtensions.cs" company="MTCS">
-// Copyright (c) MTCS 2018.
+// Copyright (c) MTCS 2024.
 // MTCS is a trading name of Meridian Technology Consultancy Services Ltd.
 // Meridian Technology Consultancy Services Ltd is registered in England and
 // Wales. Company number: 11184022.
